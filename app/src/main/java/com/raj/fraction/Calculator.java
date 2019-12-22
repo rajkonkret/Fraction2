425abad8a1385656ae4d1463016a200f261642d3
rajkonkret <rajkonkret660@gmail.com>
2019-12-22 22:04:31 +0100

    wersja dla sklepu Google

---
+++ app/src/main/java/com/raj/fraction/Calculator.java
@@ -0,0 +1,78 @@
+package com.raj.fraction;
+
+class Calculator {
+
+    Calculator() {
+    }
+
+    Fraction addFraction(Fraction first, Fraction second) {
+        int numerator;
+        int denominator;
+
+        if (second.getDenominator() == first.getDenominator()) {
+            numerator = first.getNumerator() + second.getNumerator();
+            denominator = first.getDenominator();
+        } else {
+            denominator = first.getDenominator() * second.getDenominator() / nwd(first.getDenominator(), second.getDenominator());
+            numerator = denominator / first.getDenominator() * first.getNumerator() + denominator / second.getDenominator() * second.getNumerator();
+        }
+        return new Fraction(numerator, denominator);
+    }
+
+    Fraction subFraction(Fraction first, Fraction second) {
+        int numerator;
+        int denominator;
+
+        if (second.getDenominator() == first.getDenominator()) {
+            numerator = first.getNumerator() - second.getNumerator();
+            denominator = first.getDenominator();
+        } else {
+            denominator = first.getDenominator() * second.getDenominator() / nwd(first.getDenominator(), second.getDenominator());
+            numerator = denominator / first.getDenominator() * first.getNumerator() - denominator / second.getDenominator() * second.getNumerator();
+        }
+        return new Fraction(numerator, denominator);
+    }
+
+    Fraction mulFraction(Fraction first, Fraction second) {
+        int numerator = 0;
+        int denominator = 1;
+
+        if (first.getDenominator() != 0 && second.getDenominator() != 0) {
+            numerator = first.getNumerator() * second.getNumerator();
+            denominator = first.getDenominator() * second.getDenominator();
+        }
+        return new Fraction(numerator, denominator);
+    }
+
+    Fraction divFraction(Fraction first, Fraction second) {
+        int numerator = 0;
+        int denominator = 1;
+
+        if (first.getDenominator() != 0 && second.getDenominator() != 0) {
+            numerator = first.getNumerator() * second.getDenominator();
+            denominator = first.getDenominator() * second.getNumerator();
+        }
+        return new Fraction(numerator, denominator);
+    }
+
+    int nwd(int firstN, int secondN) {
+
+        if (firstN == 0) {
+            firstN = 1;
+        }
+
+        if (firstN < 0) {
+            firstN = Math.negateExact(firstN);
+        }
+
+        while (firstN != secondN) {
+            if (firstN > secondN) {
+                firstN -= secondN;
+            } else {
+                secondN -= firstN;
+            }
+        }
+        System.out.println("wynosi: " + firstN);
+        return firstN;
+    }
+}
---
+++ app/src/main/java/com/raj/fraction/Fraction.java
@@ -0,0 +1,21 @@
+package com.raj.fraction;
+
+class Fraction {
+    private int numerator;
+    private int denominator;
+
+    Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+
+    int getNumerator() {
+        return numerator;
+    }
+
+    int getDenominator() {
+        return denominator;
+    }
+}
+
